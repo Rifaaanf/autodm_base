8ce6ec0052ef4d394307c00cbffc727f0be58902
Rif'an Fatoni Febrianto <73413289+Rifaaanf@users.noreply.github.com>
2021-01-29 22:03:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # Original code by Prieyudha Akadita S.
 #     Source: https://https://github.com/ydhnwb/autodm_base
 
-# Re-code by Rif'an Fatoni under MIT License
-#     Source: https://github.com/Rifaaanf/autodm_base
+# Re-code by Fakhri Catur Rofi under MIT License
+#     Source: https://github.com/fakhrirofi/twitter_autobase
 
 from twitter import Twitter
 from time import sleep
@@ -92,12 +92,11 @@ class Autobase:
 
                 except Exception as ex:
                     print(ex)
-                    sleep(60)
                     pass
             
             if 'idle' in indicator:
                 indicator.remove('idle')      
-            sleep(67)
+            
 
 
     def __update_dm(self, dms, indicator):
@@ -109,7 +108,7 @@ class Autobase:
             indicator.remove('dm_safe')
             dms.extend(dms_new)
             indicator.add('dm_safe')
-            sleep(65)
+            
     
 
     def update_local_file(self, sender_id, message, postid):
@@ -235,7 +234,7 @@ class Autobase:
                                 # credential.Notify_sent is False
                                 pass
                             
-                            sleep(30+self.tw.random_time)
+                            
 
                         else:
                             # Notify sender, message doesn't meet the algorithm's requirement
@@ -243,11 +242,10 @@ class Autobase:
 
                     except Exception as ex:
                         print(ex)
-                        sleep(30)
                         pass
 
             else:
-                sleep(3)
+                sleep(1)
 
             
     def check_file_github(self, new=True):
@@ -321,7 +319,7 @@ class Autobase:
                         self.AdminCmd.filename_github = f"{self.bot_username} {datee.year}-{datee.month}-{datee.day}.json"
 
                 else:
-                    sleep(60)
+                    sleep(1)
 
             except Exception as ex:
                 print(ex)
--- twitter.py
+++ twitter.py
@@ -76,13 +76,13 @@ class Twitter:
                 if first_delay is False:
                     first_delay = True
                     continue
-                sleep(60)
+                sleep(1)
             return ids
 
         except Exception as ex:
             pass
             print(ex)
-            sleep(60)
+            sleep(1)
             return list()
 
     
@@ -101,13 +101,13 @@ class Twitter:
                 if first_delay is False:
                     first_delay = True
                     continue
-                sleep(60)
+                sleep(0)
             return ids
 
         except Exception as ex:
             pass
             print(ex)
-            sleep(60)
+            sleep(1)
             return list()
     
 
@@ -340,7 +340,7 @@ class Twitter:
                         notif = self.credential.Notify_blacklistWords
                         self.send_dm(recipient_id=sender_id, text=notif)
                     except Exception as ex:
-                        sleep(60)
+                        sleep(1)
                         print(ex)
                         pass
 
@@ -412,7 +412,7 @@ class Twitter:
         except Exception as ex:
             pass
             print(ex)
-            sleep(60)
+            sleep(1)
             return dms
 
 
@@ -446,7 +446,7 @@ class Twitter:
         except Exception as ex:
             pass
             print(ex)
-            sleep(60)
+            sleep(1)
 
 
     def delete_dm(self, id):
@@ -457,7 +457,7 @@ class Twitter:
             self.api.destroy_direct_message(id)
         except Exception as ex:
             print(ex)
-            sleep(60)
+            sleep(0)
             pass
     
     
@@ -472,7 +472,7 @@ class Twitter:
         except Exception as ex:
             pass
             print(ex)
-            sleep(60)
+            sleep(0)
 
 
     def get_user_screen_name(self, id):
@@ -487,7 +487,7 @@ class Twitter:
         except Exception as ex:
             pass
             print(ex)
-            sleep(60)
+            sleep(0)
             return "Exception"
 
 
@@ -601,7 +601,7 @@ class Twitter:
         except Exception as ex:
             pass
             print(ex)
-            sleep(60)
+            sleep(0)
             return list()
 
 
@@ -697,7 +697,7 @@ class Twitter:
                         media_ids=list_media_ids[:1][0], possibly_sensitive=possibly_sensitive).id
                 
                 list_media_ids = list_media_ids[1:] + [[]]
-                sleep(30+self.random_time)
+                sleep(3)
                 # tweet are dynamic here
                 tweet = tweet[limit-separator:]
             
@@ -718,7 +718,7 @@ class Twitter:
 
             # When media_ids still exists, It will be attached to the subsequent tweets
             while len(list_media_ids[0]) != 0: # Pay attention to the list format, [[]]
-                sleep(30+self.random_time)
+                sleep(3)
 
                 print("Posting the rest of media...")
                 postid1 = self.api.update_status(
@@ -738,5 +738,5 @@ class Twitter:
         except Exception as ex:
             pass
             print(ex)
-            sleep(60)
+            sleep(0)
             return None
